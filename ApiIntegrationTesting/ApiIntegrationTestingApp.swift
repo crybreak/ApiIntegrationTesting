b05713c3f23cf015f4bcd075e2fb79fbccbadd9e
Wilfried GOSSAN <gossanguy1@gmail.com>
2024-02-14 09:57:48 +0100

    Initial Commit

---
+++ ApiIntegrationTesting/ApiIntegrationTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiIntegrationTestingApp.swift
+//  ApiIntegrationTesting
+//
+//  Created by Wilfried Mac Air on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ApiIntegrationTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
