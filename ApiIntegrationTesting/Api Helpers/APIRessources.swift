424fbcc9e7490d258acc77e2fec11d66bc1888b4
Wilfried GOSSAN <gossanguy1@gmail.com>
2024-03-11 11:58:37 +0100

    Update API ressource

--- ApiIntegrationTesting/Api Helpers/APIRessources.swift
+++ ApiIntegrationTesting/Api Helpers/APIRessources.swift
@@ -25,11 +25,12 @@ struct APIRessources: APIRessourcesProtocol {
         return urlComponents
     }
     
-    static func url(with endpoint: EndPoint) -> URL? {
-        if let url = urlComponenents.url {
-            return url.appendingPathComponent(endpoint.rawValue)
+    static func url(with endpoint: EndPoint, id: Int? = nil) -> URL? {
+        if let id = id {
+            return urlComponenents.url?.appendingPathComponent(endpoint.rawValue)
+                .appendingPathComponent(String(id))
         } else {
-            return nil
+            return urlComponenents.url?.appendingPathComponent(endpoint.rawValue)
         }
     }
     
@@ -55,34 +56,18 @@ struct APIRessources: APIRessourcesProtocol {
     }
     
     func createAlbumsPublisher() -> AnyPublisher<[Album], APIError> {
-        if let url = APIRessources.url(with: .albums) {
-           
-            return fetchUrl(url: url)
-                 .decode(type: [Album].self, decoder: JSONDecoder())
-                 .mapError({ APIError.convert(error: $0)})
-                 .eraseToAnyPublisher()
-        } else {
-            return Fail(error: APIError.badURL)
-                .eraseToAnyPublisher()
-        }
+        fetchPublisher(type: [Album].self, endPoint: .albums)
     }
     
+   
     func fetchPhotosWithQuery(searchKey: SearchKey , searchId: Int) -> AnyPublisher<[Photo], APIError> {
-        if let url = APIRessources.urlWithQuery(for: .photos, searchKey: .albumId, searchId: searchId) {
-            return fetchUrl(url: url)
-                .decode(type: [Photo].self, decoder: JSONDecoder())
-                .mapError {APIError.convert(error: $0)}
-                .eraseToAnyPublisher()
-        } else {
-           return Fail(error: APIError.badURL)
-                .eraseToAnyPublisher()
-        }
+        fetchPublisher(type: [Photo].self, endPoint: .photos, searchKey: searchKey, id: searchId)
     }
     
     func fetechImage(for photo: Photo) -> AnyPublisher<Photo, Never> {
         if let url = URL(string: photo.thumbnailImagePath) {
-            return URLSession.shared.dataTaskPublisher(for: url)
-                    .compactMap{ ( data, response) in
+            return fetchUrl(url: url)
+                    .compactMap{ ( data) in
                          UIImage(data: data)
                     }
                     .map { image -> Photo in
@@ -97,8 +82,129 @@ struct APIRessources: APIRessourcesProtocol {
                 .eraseToAnyPublisher()
         }
     }
+    
+    enum HttpMethod: String {
+        case POST, PUT, PATCH, DELETE
+    }
+    
+    //TODO: mock this
+    func fetchUserPublisher() -> AnyPublisher<[User], APIError> {
+        fetchPublisher(type: [User].self, endPoint: .users)
+    }
+    
+    //TODO: mock this
+    func fetchTodosPublisher(for userId: Int) -> AnyPublisher<[Todos], APIError> {
+        fetchPublisher(type: [Todos].self, endPoint: .todos, searchKey: .userId, id: userId)
+    }
+    
+    //MARK: Compose requests
+    
+    static func createRequest(endPoint: EndPoint, id: Int? = nil, httpMethod: HttpMethod, data: Data? = nil) -> URLRequest?  {
+        guard let url = APIRessources.url(with: endPoint, id: id) else {
+            return nil
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod.rawValue
+        if let data = data {
+            request.httpBody = data
+        }
+
+        request.addValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-type")
+        return request
+    }
+    
+    func postRequestForTodoPublisher(request: URLRequest) -> AnyPublisher<Todos, APIError> {
+        return postRequestPublisher(request: request)
+            .decode(type: Todos.self, decoder: JSONDecoder())
+            .mapError({APIError.convert(error: $0)})
+            .eraseToAnyPublisher()
+    }
+    
+    
+    
+    private func postRequestPublisher(request: URLRequest) -> AnyPublisher<Data, Error> {
+        URLSession.shared.dataTaskPublisher(for: request)
+            .tryMap { (data: Data, response: URLResponse) in
+                if let response = response as? HTTPURLResponse, response.statusCode == 201 {
+                    return data
+                } else {
+                    throw APIError.badURL
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func updatePublihser(todo: Todos) -> AnyPublisher<Todos, APIError>{
+        let encoder = JSONEncoder()
+        
+        if let data = try? encoder.encode(todo),
+           let request = APIRessources.createRequest(endPoint: .todos, id: todo.id, httpMethod: .PUT, data: data) {
+            return URLSession.shared.dataTaskPublisher(for: request)
+                .tryMap({ (data: Data, response: URLResponse) in
+                    if let response = response as? HTTPURLResponse,
+                       !(200...299).contains(response.statusCode) {
+                        throw APIError.badResponse(statusCode: response.statusCode)
+                    } else {
+                        return data
+                    }
+                })
+                .decode(type: Todos.self, decoder: JSONDecoder())
+                .mapError({APIError.convert(error: $0)})
+                .eraseToAnyPublisher()
+        } else {
+           return  Fail(error: APIError.badURL)
+                .eraseToAnyPublisher()
+        }
+    }
+     func deletePublisher(todo: Todos) -> AnyPublisher<Todos, APIError> {
+        if let request = APIRessources.createRequest(endPoint: .todos, id: todo.id, httpMethod: .DELETE) {
+            return URLSession.shared.dataTaskPublisher(for: request)
+                .tryMap({ (data: Data, response: URLResponse) in
+                    if let response = response as? HTTPURLResponse,
+                       !(200...299).contains(response.statusCode) {
+                        throw APIError.badResponse(statusCode: response.statusCode)
+                    } else {
+                        return data
+                    }
+                })
+                .decode(type: Todos.self, decoder: JSONDecoder())
+                .mapError({APIError.convert(error: $0)})
+                .eraseToAnyPublisher()
+        } else {
+           return  Fail(error: APIError.badURL)
+                .eraseToAnyPublisher()
+        }
+    }
+    
+   
 }
 
+extension APIRessources {
+    
+    private func fetchPublisher<T>(type: T.Type, endPoint: EndPoint) -> AnyPublisher<T, APIError> where T: Decodable {
+        if let url = APIRessources.url(with: endPoint) {
+            return fetchUrl(url: url)
+                .decode(type: type, decoder: JSONDecoder())
+                .mapError({APIError.convert(error: $0)})
+                .eraseToAnyPublisher()
+        } else {
+            return Fail(error: APIError.badURL)
+                .eraseToAnyPublisher()
+        }
+    }
+    
+    private func fetchPublisher<T>(type: T.Type, endPoint: EndPoint, searchKey: SearchKey, id: Int) -> AnyPublisher<T, APIError> where T: Decodable {
+        if let url = APIRessources.urlWithQuery(for: endPoint, searchKey: searchKey, searchId: id) {
+            return fetchUrl(url: url)
+                .decode(type: type, decoder: JSONDecoder())
+                .mapError({APIError.convert(error: $0)})
+                .eraseToAnyPublisher()
+        } else {
+            return Fail(error: APIError.badURL)
+                .eraseToAnyPublisher()
+        }
+    }
+}
 
 extension URLComponents {
     mutating func setQueryItems(with parameters: [String: String]) {
